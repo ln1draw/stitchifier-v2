18ed6aa62aed461998e4ff545c9a8517d1be8ddf
Lenny Wondra <hello@wondra.codes>
2023-10-23 13:11:28 -0700

    initial work on pattern component

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -4,6 +4,6 @@ class HelloWorldController < ApplicationController
   layout "hello_world"
 
   def index
-    @hello_world_props = { name: "Stranger" }
+    @hello_world_props = { sectionsWide: 5, sectionsHigh: 2, unitWidth: 10 }
   end
 end
--- app/javascript/bundles/Pattern/components/Pattern.jsx
+++ app/javascript/bundles/Pattern/components/Pattern.jsx
@@ -3,24 +3,50 @@ import React, { useState } from 'react';
 import style from './Pattern.module.css';
 
 const Pattern = (props) => {
-  const [name, setName] = useState(props.name);
+  const [
+    sectionsWide,
+    setSectionsWide
+  ] = useState(props.sectionsWide);
+  const [sectionsHigh, setSectionsHigh] = useState(props.sectionsHigh)
+  const [unitWidth, setUnitWidth] = useState(props.unitWidth)
+  const width = sectionsWide * unitWidth * 10;
+  const height = sectionsHigh * unitWidth * 10;
+  const tenUnit = unitWidth * 10;
 
   return (
     <div>
-      <h3>Hello, {name}! this is a pattern!</h3>
+      <h3>Hello, {sectionsHigh}! this is a pattern!</h3>
       <hr />
       <form>
-        <label className={style.bright} htmlFor="name">
-          Say hello to:
-          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <label className={style.bright} htmlFor="sectionsWide">
+          how many sections wide:
+          <input id="sectionsWide" type="text" value={sectionsWide} onChange={(e) => setSectionsWide(e.target.value)} />
+        </label>
+
+        <label className={style.bright} htmlFor="sectionsHigh">
+          how many sections High:
+          <input id="sectionsHigh" type="text" value={sectionsHigh} onChange={(e) => setSectionsHigh(e.target.value)} />
         </label>
       </form>
+      <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <pattern id="smallGrid" width={unitWidth} height={unitWidth} patternUnits="userSpaceOnUse">
+            <path d={"M " + unitWidth + " 0 L 0 0 0 " + unitWidth} fill="none" stroke="gray" stroke-width="0.5"/>
+            </pattern>
+            <pattern id="grid" width={tenUnit} height={tenUnit} patternUnits="userSpaceOnUse">
+            <rect width={tenUnit} height={tenUnit} fill="url(#smallGrid)"/>
+            <path d={"M " + tenUnit + " 0 L 0 0 0 " + tenUnit} fill="none" stroke="gray" stroke-width="1"/>
+            </pattern>
+        </defs>
+            
+        <rect width="100%" height="100%" fill="url(#grid)" />
+        </svg>
     </div>
   );
 };
 
 Pattern.propTypes = {
-  name: PropTypes.string.isRequired, // this is passed from the Rails view
+//   name: PropTypes.string.isRequired, // this is passed from the Rails view
 };
 
 export default Pattern;
